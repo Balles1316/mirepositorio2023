ba737b627d6cfa2fab8419756168696b00aa7d7c
Balles1316 <romanballesteros8@Hotmail.com>
2023-08-23 10:26:57 +0200

    Problemas en el menu

--- AndroidStudioProjects/studyPro/app/src/main/java/com/example/strudyapp/MainActivity.java
+++ AndroidStudioProjects/studyPro/app/src/main/java/com/example/strudyapp/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.strudyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,17 +14,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void ejecutar_info(View vista){
-
-        Intent inte = new Intent(this,InfoActivity.class);
-
-                startActivity(inte);
-
+    public void ejecutar_info(MenuItem miMenu) {
+        Intent inte = new Intent(this, InfoActivity.class);
+        startActivity(inte);
     }
 
     @Override
-    protected void onCreateOptionMenu(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_en_activity, menu);
+        return true;
     }
 }
